4c46337c5aeef9b772dd80c5eb0e502202f870d5
Nerodacles <juan.manuel.canahuare@gmail.com>
2021-01-28 14:55:22 -0400

    :+1: First Commit

---
+++ testLogin/test-login-firebase/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from "axios";
+import firebase from "firebase/app";
+
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQpj6yylxEBIN7tRRCYUGRKLKIbUaadVA",
+  authDomain: "biblioteca-vue.firebaseapp.com",
+  projectId: "biblioteca-vue",
+  storageBucket: "biblioteca-vue.appspot.com",
+  messagingSenderId: "445771738959",
+  appId: "1:445771738959:web:aecd278bf0fbd89eed1f3a",
+  measurementId: "G-GKPXCHB9JK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
