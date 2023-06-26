ceac44de0d6f8a92d50242a4ed1a2d59fb17d6ce
Nerodacles <29661633+Nerodacles@users.noreply.github.com>
2023-06-26 19:27:34 -0400

    ..

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9090
 app.listen(port)
 console.log(`BibliotecaVirtual is listening on port: ${port}`)
